0da4e268ed39aaa2b7129bf9a243d07d90e4598c
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-17 16:24:13 +0530

    spring hello world added

---
+++ src/main/java/com/example/demo/SpringDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
