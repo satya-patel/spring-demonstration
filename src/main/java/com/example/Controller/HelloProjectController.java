55a7205dff41d07c891dc542379541d64896dec8
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-17 16:32:40 +0530

    add mvc to display msg

---
+++ src/main/java/com/example/Controller/HelloProjectController.java
@@ -0,0 +1,20 @@
+package com.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloProjectController {
+	
+	@GetMapping("/web")
+	public String hello() {
+		return "hello";
+	}
+	
+	@GetMapping("/web/message")
+	public String message(Model model) {
+		model.addAttribute("messageResponse", "This is a custom message.");
+		return "message";
+	}
+}
